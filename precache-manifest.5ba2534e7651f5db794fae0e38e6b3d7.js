18da5691e76ae5cf58f0a8dffa3dae7b9ea8e56e
Toki <clanlord.ikot@gmail.com>
2020-01-30 07:33:21 +0600

    Updates

---
+++ precache-manifest.5ba2534e7651f5db794fae0e38e6b3d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ab450a02c8abed1fdc40e4c3a3e837b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5718a07c671f0f3700a",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "e649fb3572e8d6eb0584",
+    "url": "/static/js/2.db9b0887.chunk.js"
+  },
+  {
+    "revision": "241f2e221faa80b7789805592a249c46",
+    "url": "/static/js/2.db9b0887.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5718a07c671f0f3700a",
+    "url": "/static/js/main.6e557581.chunk.js"
+  },
+  {
+    "revision": "79f63987c06231517b99",
+    "url": "/static/js/runtime-main.b6a0204f.js"
+  }
+]);
